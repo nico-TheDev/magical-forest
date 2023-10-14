e7eafefc8b916a0417bdc9eb37e64498de181c68
Nico Ignacio <norbertoignaciojr@gmail.com>
2023-10-14 17:00:04 +0800

    feat: initial GUI controller

--- src/main.js
+++ src/main.js
@@ -2,9 +2,85 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
+import { Sky } from 'three/addons/objects/Sky.js';
+import GUI from 'lil-gui';  
 
 let _APP = null
 
+const TREE_TYPES = {
+    NORMAL:{
+        name:"Trees",
+        order:1,
+        size:{
+            min:100,
+            max:120,
+        },
+        center:{
+            x:0,
+            z:0
+        }
+},  
+    BIRCH:{
+        name:"Birch Trees",
+        order:2,
+        size:{
+            min:100,
+            max:130,
+        },
+        center:{
+            x:- 25,
+            z:- 0
+        }
+},  
+    PALM:{
+        name:"Palm Trees",
+        order:3,
+        size:{
+            min:130,
+            max:150,
+        },
+        center:{
+            x:25,
+            z:25
+        }
+},  
+    MAPLE:{
+        name:"Maple Trees",
+        order:4,
+        size:{
+            min:100,
+            max:120,
+        },
+        center:{
+            x:25,
+            z:25
+        }
+},  
+    PINE:{
+        name:"Pine Trees",
+        order:5,
+        size:{
+            min:200,
+            max:250,
+        },
+        center:{
+            x:0,
+            z:0
+        }
+},  
+    DEAD:{
+        name:"Dead Trees",
+        order:6,
+        size:{
+            min:100,
+            max:120,
+        },
+        center:{
+            x:25,
+            z:25
+        }
+}   
+}
 class MagicalForest {
     constructor(){
         this._Initialize();
@@ -18,6 +94,33 @@ class MagicalForest {
         this._threejs.setPixelRatio(window.devicePixelRatio);
         this._threejs.setSize(window.innerWidth,window.innerHeight);
         this._threejs.domElement.id = "threejs";
+        this._DefaultGUI = {
+            minCircleSize: 8,
+            maxCircleSize: 40,
+        }
+        this.gui = new GUI();
+        this.gui.add(this._DefaultGUI,"minCircleSize")
+                .name("Min Forest Size")
+                .onChange(value =>{
+                    this._scene.traverse(function(obj){
+                        const angle = Math.random() * Math.PI * 2
+                        const radius = this._DefaultGUI.minCircleSize + Math.random() * this._DefaultGUI.maxCircleSize
+        
+                        const x = Math.sin(angle) * radius;
+                        const z = Math.cos(angle) * radius;
+
+                        if(obj.objectName === "Forest-Tree"){
+                            obj.position.set(
+                                x + obj.treeType.center.x,
+                                0,
+                                z + obj.treeType.center.z
+                            )
+                        }
+                    }.bind(this))
+                })
+                .min(5)
+                .max(20)
+                .step(0.05)
 
         document.body.appendChild(this._threejs.domElement);
 
@@ -37,18 +140,20 @@ class MagicalForest {
 
         // MODELS
 
-        this._DefaultTrees = [];
-
         // LOADER RELATED
 
         const loadingManager = new THREE.LoadingManager();
+
+        loadingManager.onLoad = () => {
+            console.log("LOADED")
+        }
         this._loader = new GLTFLoader(loadingManager);
         const dracoLoader = new DRACOLoader();
         dracoLoader.setDecoderPath( '/examples/jsm/libs/draco/' );
         this._loader.setDRACOLoader(dracoLoader);
 
 
-        this._LoadDefaultTrees();
+        this._LoadForest();
 
         // HELPERS
 
@@ -69,11 +174,11 @@ class MagicalForest {
         light.shadow.camera.right = 100;
         light.shadow.camera.top = 100;
         light.shadow.camera.bottom = 100;
-        const directionLightHelper = new THREE.DirectionalLightHelper( light, 5 );
+        const directionLightHelper = new THREE.DirectionalLightHelper( light, 10 );
         this._scene.add(light);
         this._scene.add(directionLightHelper);
 
-        light = new THREE.AmbientLight(0x101010);
+        light = new THREE.AmbientLight("yellow",1);
         this._scene.add(light);
 
         const controls = new OrbitControls(this._camera,this._threejs.domElement);
@@ -96,30 +201,72 @@ class MagicalForest {
 
     }
 
-    _LoadDefaultTrees(){
-        this._loader.load("./resources/Trees.glb",function(model){
+    _LoadSky(){
+        const sky = new Sky();
+        sky.scale.setScalar( 10000 );
+        this._scene.add(sky);
+
+        const skyUniforms = sky.material.uniforms;
+
+		skyUniforms[ 'turbidity' ].value = 10;
+		skyUniforms[ 'rayleigh' ].value = 2;
+		skyUniforms[ 'mieCoefficient' ].value = 0.005;
+		skyUniforms[ 'mieDirectionalG' ].value = 0.8
+		const parameters = {
+			elevation: 2,
+			azimuth: 180
+		};
+
+    }
+
+    _LoadForest(){
+        this._LoadCabin();
+        this._LoadDefaultTrees(TREE_TYPES.NORMAL);
+        // this._LoadDefaultTrees(TREE_TYPES.MAPLE);
+        // this._LoadDefaultTrees(TREE_TYPES.PINE);
+        // this._LoadDefaultTrees(TREE_TYPES.BIRCH);
+    }
+
+    _LoadCabin(){
+        this._loader.load("./resources/Cabin.glb",
+        function(glb){
+            const cabin = glb.scene;
+            cabin.position.set(0,1.1,0)
+            let scale = 1 * 0.005
+            cabin.scale.set(scale,scale,scale)
+            cabin.receiveShadow = true;
+            cabin.castShadow = true;
+            this._scene.add(cabin);
+        }.bind(this));
+    }
+
+    _LoadDefaultTrees(treeType){
+    
+        this._loader.load(`./resources/${treeType.name}.glb`,
+        function(model){
             const scene = model.scene;
             const rootNode = scene.children[0];
             // ADD THE DEFAULT TREES
-
             const trees = rootNode.children.map(tree => tree);
 
-
-            for(let treeCount = 0; treeCount < 100; treeCount++){
+            for(let treeCount = 0; treeCount < 200; treeCount++){
                 const angle = Math.random() * Math.PI * 2
-                const radius = 3 + Math.random() * 40
+                const radius = this._DefaultGUI.minCircleSize + Math.random() * this._DefaultGUI.maxCircleSize
 
                 const x = Math.sin(angle) * radius;
                 const z = Math.cos(angle) * radius;
 
                 let tree = trees[Math.floor(Math.random() * 5)].clone();
-                console.log(tree.scale);
-                let size = Math.random() * (120 - 100) + 100;
+
+                tree.objectName = "Forest-Tree"
+                tree.treeType = treeType;
+                tree.renderOrder = treeType.order
+                let size = Math.random() * (treeType.size.max - treeType.size.min) + treeType.size.min;
                 tree.scale.set(size,size,size);
                 tree.position.set(
-                    x,
+                    x + treeType.center.x,
                     0,
-                    z
+                    z + treeType.center.z
                 )
                 tree.castShadow = true;
                 tree.receiveShadow = true;
